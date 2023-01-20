1b4f48a756bd3be595e22fd4e28a10f509154992
gustavo rocha <pereiragustavo215@gmail.com>
2023-01-20 10:19:41 -0300

    tratando click e up button

--- app/src/main/java/dominando/android/navigationapi/HomeFragment.kt
+++ app/src/main/java/dominando/android/navigationapi/HomeFragment.kt
@@ -1,17 +1,42 @@
 package dominando.android.navigationapi
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import dominando.android.navigationapi.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ) = FragmentHomeBinding.inflate(inflater, container, false).apply {
+        _binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //essa forma é melhor usado para quando o navigation está dentro do click de botao
+        binding.btnGo.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_completeFragment)
+        )
+
+        // forma mais generica
+        /*binding.btnGo.setOnClickListener {
+            Navigation.findNavController(requireActivity(), R.id.navHostFragment)
+                .navigate(R.id.action_homeFragment_to_completeFragment)
+        }*/
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
